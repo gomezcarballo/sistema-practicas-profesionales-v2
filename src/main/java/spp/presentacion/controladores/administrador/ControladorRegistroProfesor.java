1f85137acb2093382a265f070ddfd9448856b27e
gomezcarballo <vicgomezcarballo@gmail.com>
2026-04-26 22:24:15 -0600

    feat: diseño guis de coord. prof. y org

--- src/main/java/spp/presentacion/controladores/administrador/ControladorRegistroProfesor.java
+++ src/main/java/spp/presentacion/controladores/administrador/ControladorRegistroProfesor.java
@@ -4,10 +4,46 @@
  */
 package spp.presentacion.controladores.administrador;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import spp.logicadenegocio.clasesdto.Profesor;
+
 /**
  *
  * @author gomes
  */
 public class ControladorRegistroProfesor {
+    
+    @FXML
+    private TextField ingresaNombre;
+    
+    @FXML
+    private TextField ingresaApellidoPaterno;
+    
+    @FXML
+    private TextField ingresaApellidoMaterno;
+    
+    @FXML
+    private TextField ingresaNoPersonal;
+       
+    @FXML
+    private Button botonCancelar;
+    
+    @FXML
+    private void leerProfesor(){
+        
+        String nombre = ingresaNombre.getText();
+        String apellidoPaterno = ingresaApellidoPaterno.getText();
+        String apellidoMaterno = ingresaApellidoMaterno.getText();
+        String noPersonal = ingresaNoPersonal.getText();
+        
+        Profesor profesor = new Profesor();
+        profesor.setNombre(nombre);
+        profesor.setApellidoPaterno(apellidoPaterno);
+        profesor.setApellidoMaterno(apellidoMaterno);
+        profesor.setNumeroDePersonal(noPersonal);
+       
+    }
     
 }
